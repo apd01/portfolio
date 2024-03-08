1f14b45e75c956314f9ce1d4f244b83536bc06ce
apd01 <alandunne85@gmail.com>
2024-03-08 20:10:47 +0000

    Update .bat

---
+++ commit_site.bat
@@ -0,0 +1,24 @@
+@echo off
+REM Switch to the new branch, creating it if it doesn't exist
+git checkout -b github-pages
+
+REM Remove all files tracked by git 
+git rm -rf .
+
+REM Move the dist/ content to the root
+xcopy /E /I /H dist .
+
+REM Add all the files in the current directory to the staging area
+git add .
+
+REM Commit the changes
+git commit -m "Update GitHub Pages"
+
+REM Push the branch to the remote repository
+git push -u origin github-pages
+
+REM Switch back to the main branch
+git checkout main
+
+REM Pull the latest changes from main
+git pull origin main
